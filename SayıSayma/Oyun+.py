15dfdf1bbf9c0b8e3be3ce9a0720694b003d410c
Afflicted <yagizaltan5257@gmail.com>
2023-08-03 11:58:50 +0300

    denemeler

---
+++ SayıSayma/Oyun+.py
@@ -0,0 +1,11 @@
+
+print("""Sayı tahmin oyununa hoş geldiniz
++sayı 1-100 arasında olsun istiyorsanız a/A giriniz
++sayı 1-1000 arasında olsun istiyorsanız b/B giriniz""")
+x = input("")
+
+
+if x == "a" or x == "A":
+    import yüz
+elif x == "b" or x == "B":
+    import bin
---
+++ SayıSayma/bin.py
@@ -0,0 +1,55 @@
+import random
+import time
+
+print("Sayı tahmin oyununa hoş geldiniz!n 1-1000 arasında bir sayı tahmin edin")
+sayi = random.randint(1,1000)
+tahmin_hakki = 10
+
+while True:
+    tahmin = int(input("Tahmininiz: "))
+    
+    if tahmin == sayi:
+        print("Sayı sorgulanıyor... ")
+        time.sleep(1)
+        print("Tebrikler! doğru bildiniz")
+
+        devam=input("""tekrar oynamak ister misiniz?
+        eğer oynamak isterseniz d/D tuşlayınız
+        kapatmak isterseniz k/K tuşlayınız""")
+        if devam=="d" or devam=="D":
+            tahmin_hakki = 10
+            continue
+        else:
+            print("byeee")
+            break
+        
+
+    elif tahmin > sayi:
+        print("Sayı sorgulanıyor... ")
+        time.sleep(1)
+        tahmin_hakki-=1
+        print("Daha küçük bir sayı giriniz")
+        print("Kalan tahmin hakkı: ",tahmin_hakki)
+
+        
+    else:
+        print("Sayı sorgulanıyor... ")
+        time.sleep(1)
+        tahmin_hakki -= 1
+        print("Daha büyük bir sayı giriniz")
+        print("Kalan tahmin hakkı: ", tahmin_hakki)
+    if tahmin_hakki == 0:
+        print("Tahmin hakkınız bitmiştir")
+        print("Bilgisayarın tahmini: ",sayi)
+
+
+        
+        devam=input("""tekrar oynamak ister misiniz?
+        eğer oynamak isterseniz d/D tuşlayınız
+        kapatmak isterseniz k/K tuşlayınız""")
+        if devam=="d" or devam=="D":
+            tahmin_hakki = 10
+            continue
+        else:
+            print("byeee")
+            break
